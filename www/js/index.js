6f2e36925b49dc27fdf1288900cbba2352c06e24
Judson Cooper <jcooper@hightowerservices.com>
2015-07-10 09:15:57 -0500

    Updating app with new files

--- www/js/index.js
+++ www/js/index.js
@@ -17,50 +17,16 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-		$('.cameraButton').click( function() {
-			//open camera
-			navigator.camera.getPicture(onSuccess, onFail, {quality: 50,
-				destinationType: Camera.DestinationType.DATA_URL
-			});
-			
-			function onSuccess(imageData) {
-				var image = document.getElementById('myImage');
-				image.src = "data:image/jpeg;base64," + imageData;
-				$('#myImage').fadeIn();
-			}
-			
-			function onFail(message) {
-				$('#myImage').before('<div class="alert alert-danger">' + message + '</div>');
-				
-			}
-		})
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+        //wait 2 seconds and forward to program
+		setTimeout(function(){ 
+			window.location.replace("program.html");
+		}, 2000);
     }
 };
